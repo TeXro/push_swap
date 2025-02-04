c8bdc13b61605a8a99e9f99597cf6f67c3e8ccc1
TeXro <84862117+TeXro@users.noreply.github.com>
2025-02-04 10:31:58 +0100

    f

--- main.c
+++ main.c
@@ -26,24 +26,26 @@ void new_node(s_list **stack, int num)
 	tmp->next = node;
 }
 
-int handle_arg(char *arg, s_list **stack)
+int handle_arg(char **arg, s_list **stack)
 {
 	int i;
+	int c;
 	char **sp_arg;
 
+	c = 0;
 	i = 0;
 	while (arg[i])
 	{
-		if ((arg[i] > '9' || arg[i] < '0') && arg[i] != ' ' && arg[i] != '-')
-			return 0;
+		sp_arg = ft_split(arg[i], ' ');
+		// if ((arg[i] > '9' || arg[i] < '0') && arg[i] != ' ' && arg[i] != '-')
+		// 	return 0;
 		i++;
-	}
-	sp_arg = ft_split(arg, ' ');
-	i = 0;
-	while (sp_arg[i])
+	c = 0;
+	while (sp_arg[c])
 	{
-		new_node(stack, ft_atoi(sp_arg[i]));
-		i++;
+		new_node(stack, ft_atoi(sp_arg[c]));
+		c++;
+	}
 	}
 	return 1;
 }
@@ -54,25 +56,14 @@ int main(int argc, char **argv)
 	s_list *stacka = NULL;
 	if (argc == 1)
 		return ft_error();
-	int c = 0;
-	printf("<sp_arg>\n");
-	while (argv[a])
-	{
-		if(handle_arg(argv[a], &stacka) == 0)
-			return ft_error();
-		a++;
-	}
+	if(handle_arg(argv + 1, &stacka) == 0)
+		return ft_error();
 	s_list *currenta = stacka;
-	s_list *currents = stacka;
 	printf("<stackA>\n");
     while (currenta)
     {
 		
         printf("[%d]\n", currenta->data);
         currenta = currenta->next;
-		if (currenta && (stacka->data == currenta->data))
-		{
-			printf("match\n%d = %d", stacka->data, currenta->data);
-		}
     }
 }
